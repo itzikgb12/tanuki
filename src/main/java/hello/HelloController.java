25a29cedd1c1b1b3b3c1156cd94df0d31fd505a4
Itzik Gan Baruch <iganbarouch@gitlab.com>
2020-10-12 11:11:42 +0000

    Hello world

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
         style += "body { background-color: #a6b9d8; position: fixed; top: 30%; left: 50%; transform: translate(-50%, -50%); color: white; font-size: 250%; }";
         style += "</style>";
         
-        String message = "Hello from GitLab via Java Maven";
+        String message = "Hello world!";
         String message2 = "<BR>Go to our <a href='/'>home</a> page";
         
         String body = "<body>" + message  + "</body>";
